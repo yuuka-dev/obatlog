23ce37be3a38ba390d4beab2fc2d460c2c7daaf3
Yumeno Yuuka <sounyan.is.the.angel@gmail.com>
2026-03-25 22:28:03 +0900

    chore: jest カバレッジ設定 + CI レポート出力（#11）

--- functions/jest.config.js
+++ functions/jest.config.js
@@ -2,4 +2,14 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
